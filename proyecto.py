4e64390325c52111d87fb4e9760b1e45e34220f6
vic-gar <vagarciag05@gmail.com>
2024-08-30 20:41:35 -0600

    Add files via upload

--- proyecto.py
+++ proyecto.py
@@ -1,31 +1,86 @@
-print("1. Cuando estoy en una reunion de trabajo \n 1) Prefiero pasar desapercibido \n 2) Intervengo solo cuando es necesario \n 3) Me gusta participar")
+'''
+El proyecto se basa en un gestor de inventario para tiendas, actualmente el \
+proyecto solo cuenta con 5 productos máximo, se pedirán el nombre del \
+producto al usuario, la cantidad de ese mismo producto y el costo \
+por unidad del producto. Con estos datos se otorgarán datos como el costo del \
+inventario y el total de productos.
+'''
+'''
+Inicio del programa
+'''
+'''
+Se define una función para calcular el costo del inventario, multiplicando la \
+cantidad de productos de un tipo por el costo por unidad del mismo producto, y \
+posteriormente sumando los resultados de cada uno de los productos dados.
+'''
 
-resp_1 = int(input())
-resultado = resp_1
+def costo_inventario(prod_1, cos_1, prod_2, cos_2, prod_3, cos_3, prod_4, \
+cos_4, prod_5, cos_5):
+     val_1 = prod_1 * cos_1
+     val_2 = prod_2 * cos_2
+     val_3 = prod_3 * cos_3
+     val_4 = prod_4 * cos_4
+     val_5 = prod_5 * cos_5
+     costo = val_1 + val_2 + val_3 + val_4 + val_5
+     return costo
 
-print("2. A la hora de entablar nuevas amistades \n 1) No me gusta, y me cuesta muchísimo \n 2) Dependiendo de cómo sean las personas, se me da mejor o peor \n 3) Me gusta y lo hago fácilmente")
+'''
+Se define una segunda función para calcular el total de productos sumando la \
+cantidad dada de cada uno de los productos.
+'''
 
-resp_2 = int(input())
-resultado = resp_1 + resp_2
+def total_productos(prod_1, prod_2, prod_3, prod_4, prod_5):
+    productos = prod_1 + prod_2 + prod_3 + prod_4 + prod_5
+    return productos
 
-print("3. Mi interés y disfrute por las relaciones sociales \n 1) Es nulo \n 2) Es moderado, dependiendo de las circunstancias \n 3) Es alto; me encanta estar con gente, hablar, etcétera")
+'''
+En esta etapa se le indica al usuario que coloque la totalidad de productos \
+con su nombre, total de productos que hay actualmente y su precio por unidad. \
+Ahora solo utilizaremos 5 productos en total.
+'''
 
-resp_3 = int(input())
-resultado = resp_1 + resp_2 + resp_3
+print("Coloca el nombre del producto 1, su cantidad y su precio por unidad")
+nom_producto_1 = str(input())
+cantidad_producto_1 = int(input())
+costo_producto_1 = int(input())
 
-print ("4. En general, mi grado de sociabilidad es \n 1) Bajísimo \n 2) Moderado \n 3) Muy alto")
+print("Coloca el nombre del producto 2, su cantidad y su precio por unidad")
+nom_producto_2 = str(input())
+cantidad_producto_2 = int(input())
+costo_producto_2 = int(input())
 
-resp_4 = int(input())
-resultado = resp_1 + resp_2 + resp_3 + resp_4
+print("Coloca el nombre del producto 3, su cantidad y su precio por unidad")
+nom_producto_3 = str(input())
+cantidad_producto_3 = int(input())
+costo_producto_3 = int(input())
 
-print ("5. Cuando debo de realizar un trabajo en equipo  \n 1) Lo evito como sea \n 2) Lo hago, aunque no sea mi elección preferida \n 3) Me gusta organizarme y trabajar con la gente")
+print("Coloca el nombre del producto 4, su cantidad y su precio por unidad")
+nom_producto_4 = str(input())
+cantidad_producto_4 = int(input())
+costo_producto_4 = int(input())
 
-resp_5 = int(input())
-resultado = int(resp_1 + resp_2 + resp_3 + resp_4 + resp_5)
+print("Coloca el nombre del producto 5, su cantidad y su precio por unidad")
+nom_producto_5 = str(input())
+cantidad_producto_5 = int(input())
+costo_producto_5 = int(input())
 
-print ("Si tu resultado se encuentra entre 1 y 5 eres INTROVERTIDO")
-print ("Si tu resultado se encuentra entre 6 y 10 eres AMBIVERTIDO")
-print ("Si tu resultado se encuentra entre 11 y 15 eres EXTROVERTIDO")
+'''
+Para finalizar, se mandan los datos dados por el usuario a las fuciones \
+del inicio para calcular el total de productos y el costo del inventario \
+actual. Luego se imprime el resultado al usuario.
+'''
 
-print("Resultado:", resultado)
+inventario = costo_inventario(cantidad_producto_1, costo_producto_1, \
+cantidad_producto_2, costo_producto_2, cantidad_producto_3, costo_producto_3, \
+cantidad_producto_4, costo_producto_4, cantidad_producto_5, costo_producto_5)
 
+print("El costo total del inventario es:", inventario)
+
+productos = total_productos(cantidad_producto_1, cantidad_producto_2, \
+cantidad_producto_3, cantidad_producto_4, cantidad_producto_5)
+
+print("El total de productos en el inventrio es:", productos)
+
+'''
+Fin del programa
+'''
