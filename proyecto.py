a919674c43c4e01d456e2583e8f14c206160341f
vic-gar <vagarciag05@gmail.com>
2024-09-05 20:55:47 -0600

    Add files via upload

--- proyecto.py
+++ proyecto.py
@@ -34,6 +34,25 @@ def total_productos(prod_1, prod_2, prod_3, prod_4, prod_5):
     return productos
 
 '''
+Esta función indica el precio a mayoreo dependiendo de la cantidad del \
+producto comprado, siendo todos el mismo producto, y su costo unitario, \
+así, podemos indicar que si se compró entre 20  y 49 productos se aplica \
+un descuento del 15%, si compras más de 49 se hace un descuento del 25%
+'''
+
+def calcular_descuento_mayoreo(cantidad_producto,costo_producto):
+    if cantidad_producto >= 50:
+        descuento = 0.25
+    elif cantidad_producto >= 20 and cantidad_producto < 50:
+        descuento = 0.15
+    else:
+        descuento = 0
+    costo = cantidad_producto * costo_producto
+    costo_descuento = costo * (1 - descuento)
+    return costo_descuento
+
+
+'''
 En esta etapa se le indica al usuario que coloque la totalidad de productos \
 con su nombre, total de productos que hay actualmente y su precio por unidad. \
 Ahora solo utilizaremos 5 productos en total.
@@ -64,22 +83,32 @@ nom_producto_5 = str(input())
 cantidad_producto_5 = int(input())
 costo_producto_5 = int(input())
 
+
 '''
 Para finalizar, se mandan los datos dados por el usuario a las fuciones \
-del inicio para calcular el total de productos y el costo del inventario \
-actual. Luego se imprime el resultado al usuario.
+del inicio para calcular el total de productos, el costo del inventario \
+actual y el descuento por mayoreo. Luego se imprime el resultado al usuario.
 '''
 
-inventario = costo_inventario(cantidad_producto_1, costo_producto_1, \
+inventario_total_costo = costo_inventario(cantidad_producto_1, costo_producto_1, \
 cantidad_producto_2, costo_producto_2, cantidad_producto_3, costo_producto_3, \
 cantidad_producto_4, costo_producto_4, cantidad_producto_5, costo_producto_5)
 
-print("El costo total del inventario es:", inventario)
+print("El costo total del inventario es:", inventario_total_costo)
 
 productos = total_productos(cantidad_producto_1, cantidad_producto_2, \
 cantidad_producto_3, cantidad_producto_4, cantidad_producto_5)
 
 print("El total de productos en el inventrio es:", productos)
+
+print("Indica la cantidad de producto para el descuento por mayoreo y el costo \
+por unidad de ese producto")
+cantidad_producto_may = int(input())
+costo_producto_may = float(input())
+costo_mayoreo = calcular_descuento_mayoreo(cantidad_producto_may, \
+costo_producto_may)
+
+print("El costo con descuento es:", costo_mayoreo)
 
 '''
 Fin del programa
